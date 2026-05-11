e1f6d3b9290fbbc10f5e0ea124585561b1e4acc6
nieda0605 <281086729+nieda0605@users.noreply.github.com>
2026-05-12 05:47:09 +0900

    bootstrapの導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jqery3
+//= require popper
+//= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
