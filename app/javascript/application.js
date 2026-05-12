3618f13f287cc02239a3c7898a9d4d7ac24d8339
nieda0605 <281086729+nieda0605@users.noreply.github.com>
2026-05-13 05:27:35 +0900

    トップページのレイアウト変更中

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jqery3
+//= require jquery3
 //= require popper
 //= require bootstrap
 import "@hotwired/turbo-rails"
