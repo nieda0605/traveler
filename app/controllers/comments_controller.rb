ea8c693b11bdc81a0ab7a949667739497a4f0b33
nieda0605 <281086729+nieda0605@users.noreply.github.com>
2026-06-05 05:45:48 +0900

    コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
     def create
         post = Post.find(params[:post_id])
         comment = current_user.comments.new(comment_params)
-        comment.post_id = post_id
+        comment.post_id = post.id
         comment.save
         redirect_to post_path(post)
     end
